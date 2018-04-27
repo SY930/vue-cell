9f766537acd02109047ecdf9ea42f461039b4535
SY930 <867598193@qq.com>
2018-04-27 18:03:42 +0800

    导航，样式，路由，API

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,66 @@
 <template>
-  <div id="app">
-    <Header/>
-    <div class="tab">
-      <div class="tab-item">商品</div>
-      <div class="tab-item">评价</div>
-      <div class="tab-item">商家</div>
+  <div>
+    <Header :seller="seller"/>
+    <div class="tab border_1px">
+      <div class="tab-item">
+        <router-link to="/goods">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/ratings">评价</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">商家</router-link>
+      </div>
     </div>
-    <div class="content">
-      content
-    </div>
+    <router-view></router-view>
   </div>
 </template>
 
-<script >
+<script>
   import Header from './components/header/header.vue'
+  import {getGoods} from './api'
+  const ERROR = 0;
   export default{
-      components:{Header}
+
+      data(){
+        return{
+            seller:{}
+        }
+      },
+    created(){
+         this.getData();
+    },
+    methods:{
+      getData(){
+        getGoods().then((res)=>{
+          if(res.error==ERROR){
+            this.seller = res.data;
+          }
+
+        })
+      }
+    },
+    components: {Header}
   }
 </script>
 
 <style type="text/stylus" lang="stylus">
-  #app
-      .tab
-          display :flex
-          width: 100%
-          height: 40px
-          line-height: 40px
-      .tab-item
-          flex :1
-          text-align :center
+  @import "./common/stylus/mixin.styl"
+  .tab
+    display: flex
+    width: 100%
+    height: 40px
+    line-height: 40px
+    border-1px(rgba(7, 17, 27, .1))
+
+    .tab-item
+      flex: 1
+      text-align: center
+
+      a
+        display: block
+        font-size: 14px
+        color: rgb(77, 85, 93)
+        &.router-link-active
+          color: rgb(240, 20, 20)
 </style>
