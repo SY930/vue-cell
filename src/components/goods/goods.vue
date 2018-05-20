36437313c5b212bfdef8bfd09a2544ebffd8b315
SN <867598193@qq.com>
2018-05-20 10:55:16 +0800

    应用小球和购物车

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -2,7 +2,7 @@
   <div class="goods">
     <div class="menu-wrapper" ref="menuWrapper">
       <ul>
-        <li v-for="item in goods" class="menu-item">
+        <li v-for="(item,index) in goods" class="menu-item" :class="{'current':currentIndex===index}" @click="selectMenu(index,$event)">
           <span class="text border_1px">
            <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span> {{item.name}}
           </span>
@@ -27,24 +27,34 @@
                 <div class="price">
                   <span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
                 </div>
+              <div class="cartcontrol-wrapper">
+                <cartControl :food="food" ></cartControl>
+              </div>
               </div>
             </li>
           </ul>
         </li>
       </ul>
     </div>
-  </div>
 
+   <shopCart :delivery-price="seller.deliveryPrice" :min-price="seller.minPrice" :select-foods="selectFoods" ref="shopCart"></shopCart>
+  </div>
 </template>
 <script>
   import {getGoods} from '../../api'
   import BScroll from 'better-scroll'
+  import shopCart from '../../components/shopCart/shopcart.vue'
+  import cartControl from '../../components/cartcontrol/cartcontrol.vue'
   const ERR_OK = 0;
   export default{
       props:{
           seller:{
               type:Object
           }
+      },
+      components:{
+        shopCart,
+        cartControl
       },
       data(){
         return {
@@ -54,20 +64,39 @@
         }
       },
     computed:{
+          //左侧当前的索引应该在哪  对比scrollY和lineheight的值 返回在哪个区域的索引
       currentIndex(){
-        for (let i = 0; i < this.listHeight.length; i++) {
-           let height1 = this.listHeight[i];
+        for(let i = 0; i < this.listHeight.length; i++) {
+          let height1 = this.listHeight[i];
            let height2 = this.listHeight[i+1];
-            if(!height2 || (this.scrollY>height1&&this.scrollY<height2)){
-                return i;
-            }
-            return 0
+          if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+            return i;
+          }
+
         }
+        return 0;
+      },
+      selectFoods(){
+          let foods=[];
+          this.goods.forEach((good)=>{
+              good.foods.forEach((food)=>{
+                  if(food.count){
+                      foods.push(food)
+                  }
+
+              })
+          })
+        return foods
       }
     },
       created(){
         this.getDate();
-        this.classMap = ['decrease', 'discount', 'guarantee', 'invoice', 'special']
+        this.classMap = ['decrease', 'discount', 'guarantee', 'invoice', 'special'];
+        //父组件接收到cartcontral传来的cart.add事件，当父组件拿到这个事件之后调用子组件的方法即（shortcart中定义的drop方法）这里处理下落函数
+        this.$root.eventHub.$on('cart.add', (event)=>{
+         this._drop(event)
+
+        } )
 
       },
       methods:{
@@ -76,9 +105,9 @@
                 if(res.errno===ERR_OK){
                     this.goods = res.data;
                     this.$nextTick(()=>{
-                      //实现滚动效果
+                      //实现滚动效果  滚动时实时记录scrollY的值
                       this._initScroll();
-                      //实现联动效果
+                      //实现联动效果 记录每个区域的高度lineheight
                       this._calculateHeight();
                     });
 
@@ -89,25 +118,43 @@
           },
           _initScroll(){
               //接收两个参数一个是dom对象，一个是option对象（json）
-              this.meunScroll = new BScroll(this.$refs.menuWrapper,{});
-            this.foodScroll = new BScroll(this.$refs.foodsWrapper,{
-                probeTYpe:3//希望bscroll在滚动的时候，能实时告诉我们位置
+              this.meunScroll = new BScroll(this.$refs.menuWrapper,{
+                  //点击没有效果，是因为bscroll监听了一些touchstart和touchend事件，会阻止默认事件，需要传属性click，在pc端是没有阻止的，会触发两次点击事件，解决方法：传入event事件if(!event._constructed){return}
+                  click:true
+              });
+            this.foodsScroll = new BScroll(this.$refs.foodsWrapper,{
+              click:true,
+                probeType:3//希望bscroll在滚动的时候，能实时告诉我们位置
             });
-            this.foodScroll.on('scroll',(pos)=>{
-              console.log(pos.y);
+            this.foodsScroll.on('scroll',(pos)=>{//事件回调函数的参数是一个位置
+              //console.log(pos.y);
               this.scrollY = Math.abs(Math.round(pos.y));
             })
           },
         _calculateHeight(){
-          let foodList = this.$refs.foodWrapper.getElementsByClassName('food-list-hook');
+          let foodList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook');
           let height = 0;
           this.listHeight.push(height);
           for (let i = 0; i < foodList.length; i++) {
             let item = foodList[i];
             height+=item.clientHeight;
             this.listHeight.push(height);
-
+            //console.log(this.listHeight);
           }
+
+        },
+        selectMenu(index,event){
+          if(!event._constructed){
+             return
+         }
+         let foodList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook');
+          let el = foodList[index];
+          this.foodsScroll.scrollToElement(el,2000);
+        },
+        _drop(event){
+        //子组件发射(cart_add)事件，父组件监听到事件之后把target传进来，再调用shopcart的（drop）方法,这样就实现了把cartcontral里面的dom元素(点击的那个元素)传递给了父组件，然后父组件调用子组件shopcart方法，把target传递到子组件，所以子组件就可以拿到这个元素，我们就可以获取这个(cartcontral)元素的位置了
+         // console.log(event.target);
+          this.$refs.shopCart.drop(event.target)
         }
 
       }
@@ -132,6 +179,14 @@
        width :56px
        line-height :14px
        padding: 0 12px;
+       &.current
+         position: relative;
+         z-index: 10
+         margin-top :-1px
+         background :#fff
+         font-weight :700
+         .text
+            border-none()
        .icon
            display :inline-block
            width :12px
@@ -208,4 +263,8 @@
              color :rgb(147,153,159)
 
 
+         .cartcontrol-wrapper
+           position: absolute;
+           right :0
+           bottom :12px
 </style>
