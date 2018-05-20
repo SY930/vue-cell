ce3f0dc18e5df81e73216b6ff54110961545a86c
SN <867598193@qq.com>
2018-05-20 10:54:32 +0800

    注册事件

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import App from './App';
+import App from './App.vue';
 import router from './router';
 import './common/stylus/index.styl'
 
@@ -11,6 +11,10 @@ Vue.config.productionTip = false;
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  render: h => h(App),
+  /*components: { App },
+  template: '<App/>'*/
+  data: {
+    eventHub: new Vue()
+  },
 });
