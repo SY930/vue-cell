c751d23e16eb680c64b2be872f73be32cdd33807
SN <867598193@qq.com>
2018-05-20 10:53:47 +0800

    购物车组件

---
+++ src/components/shopCart/shopcart.vue
@@ -0,0 +1,272 @@
+<template>
+  <div class="shopcart">
+    <div class="content">
+      <div class="content-left">
+        <div class="logo-wrapper">
+          <div class="logo" :class="{'highLight':totalCount>0}">
+            <i class="icon-shopping_cart" :class="{'highLight':totalCount>0}"></i>
+          </div>
+          <div class="num" v-show="totalCount>0">
+            {{totalCount}}
+          </div>
+        </div>
+        <div class="price" :class="{'highLight':totalPrice>0}">￥{{totalPrice}}元</div>
+        <div class="desc">另需配送费￥{{deliveryPrice}}元</div>
+      </div>
+      <div class="content-right">
+        <div class="pay" :class="payClass">
+          {{payDesc}}
+        </div>
+      </div>
+    </div>
+
+    <!--动画中的小球-->
+      <div class="ball-container">
+        <div v-for="ball in balls" >
+        <transition
+          name="drop" @before-enter="beforeDrop" @enter="dropping" @after-enter="afterDrop"
+        >
+      <div  v-show="ball.show" class="ball">
+        <div class="inner inner-hook"></div>
+      </div>
+        </transition>
+        </div>
+      </div>
+
+
+
+  </div>
+</template>
+<script>
+  export default {
+    props: {
+      selectFoods: {
+        type: Array,
+        default(){
+          return [{
+            count: 2,
+            price: 30
+          }]
+        }
+      },
+      deliveryPrice: {
+        type: Number,
+        //default:0
+      },
+      minPrice: {
+        type: Number,
+        default: 0
+      }
+    },
+    data(){
+      return {
+        balls: [
+          {show: false},
+          {show: false},
+          {show: false},
+          {show: false},
+          {show: false},
+        ],
+        dropBalls:[]
+      }
+    },
+    created(){
+
+    },
+    methods: {
+        //el是点击添加的那个按钮
+        drop(el){
+          //console.log(el);
+          for(let i=0;i<this.balls.length;i++){
+              let ball = this.balls[i];
+              if(!ball.show){
+                  ball.show = true;//关键点：能触发动画
+                  ball.el = el;//保留dom元素，算位置的时候用
+                this.dropBalls.push(ball);
+                return
+              }
+          }
+        },
+      beforeDrop(el) {
+        //console.log(el);//小球的dom
+
+        let count = this.balls.length;
+        while (count--) {
+          let ball = this.balls[count];
+          if (ball.show) {
+           // console.log(ball.el);//点击的事件源
+            let rect = ball.el.getBoundingClientRect();//获取元素相对于视口的位置；rect返回的left和top就是相对视口的偏移
+            let x = rect.left - 32;
+            let y = -(window.innerHeight - rect.top - 22);
+            el.style.display = '';
+            el.style.webkitTransform = `translate3d(0,${y}px,0)`;
+            el.style.transform = `translate3d(0,${y}px,0)`;
+            let inner = el.getElementsByClassName('inner-hook')[0];
+            inner.style.webkitTransform = `translate3d(${x}px,0,0)`;
+            inner.style.transform = `translate3d(${x}px,0,0)`;
+          }
+        }
+      },
+      dropping(el, done) {
+        /* eslint-disable no-unused-vars */
+        let rf = el.offsetHeight;//主动触发浏览器重绘。加上注释才不会报错
+        this.$nextTick(() => {
+          el.style.webkitTransform = 'translate3d(0,0,0)';
+          el.style.transform = 'translate3d(0,0,0)';
+          let inner = el.getElementsByClassName('inner-hook')[0];
+          inner.style.webkitTransform = 'translate3d(0,0,0)';
+          inner.style.transform = 'translate3d(0,0,0)';
+          el.addEventListener('transitionend', done);
+        });
+      },
+      afterDrop(el) {
+        let ball = this.dropBalls.shift();
+        if (ball) {
+          ball.show = false;
+          el.style.display = 'none';
+        }
+      }
+
+
+    },
+    computed: {
+      totalPrice(){
+        let total = 0;
+        this.selectFoods.forEach((food) => {
+          total += food.count * food.price
+        });
+        return total
+      },
+      totalCount(){
+        let count = 0;
+        this.selectFoods.forEach((food) => {
+          count += food.count
+        });
+        return count
+      },
+      payDesc(){
+        if (this.totalPrice === 0) {
+          return `￥${this.minPrice}元起送`
+        } else if (this.totalPrice < this.minPrice) {
+          let diff = this.minPrice - this.totalPrice
+          return `还差￥${diff}元起送`
+        } else {
+          return '去结算'
+        }
+      },
+      payClass(){
+        if (this.totalPrice < this.minPrice) {
+          return 'not-enough'
+        } else {
+          return 'enough'
+        }
+      }
+
+    },
+    mounted(){
+    }
+  }
+</script>
+<style scoped lang="stylus" type="text/stylus">
+  .shopcart
+    position: fixed;
+    left: 0;
+    bottom: 0
+    z-index: 50
+    width: 100%
+    height: 48px
+    .content
+      display: flex
+      background: #141d27
+      font-size: 0
+      color: rgba(255, 255, 255, .4)
+      .content-left
+        flex: 1
+        .logo-wrapper
+          display: inline-block
+          vertical-align: top
+          position: relative;
+          top: -10px
+          margin: 0 12px
+          padding: 6px
+          width: 56px
+          height: 56px
+          box-sizing: border-box
+          border-radius: 50%
+          background: #141d27
+          .logo
+            width: 100%
+            height: 100%
+            border-radius: 50%
+            text-align: center
+            background: #2d343c;
+            &.highLight
+              background: rgb(0, 160, 220)
+            .icon-shopping_cart
+              line-height: 44px
+              font-size: 24px
+              color: #80858a
+              &.highLight
+                color: #fff
+          .num
+            position: absolute;
+            top: 0
+            right: 0
+            width: 24px
+            height: 16px
+            line-height: 16px
+            text-align: center
+            border-radius: 16px
+            font-size: 9px
+            font-weight: 700
+            color: #ffffff
+            background: rgb(240, 20, 20)
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.4)
+        .price
+          display: inline-block
+          vertical-align: top
+          margin-top: 12px
+          line-height: 24px
+          padding-right: 12px
+          box-sizing: border-box
+          border-right: 1px solid rgba(255, 255, 255, .1)
+          font-size: 16px
+          font-weight: 700
+          &.highLight
+            color: #fff
+        .desc
+          display: inline-block
+          vertical-align: top
+          margin-left: 12px
+          margin-top: 12px
+          line-height: 24px
+          font-size: 14px
+      .content-right
+        flex: 0 0 105px
+        width: 105px
+        .pay
+          height: 48px
+          line-height: 48px
+          text-align: center
+          font-size: 12px
+          font-weight: 700
+          background: #2b333b
+          &.not-enough
+            background: #2b333b
+          &.enough
+            background: #00b43c
+            color: #fff
+  .ball-container
+    .ball
+      position: fixed
+      left: 32px
+      bottom: 22px
+      z-index: 200
+      transition: all 0.4s cubic-bezier(0.49, -0.29, 0.75, 0.41)
+      .inner
+        width: 16px
+        height: 16px
+        border-radius: 50%
+        background: rgb(0, 160, 220)
+        transition: all 0.4s linear
+</style>
