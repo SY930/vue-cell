5c00b5d38273676c6518141c1ebd56af71a5541c
SN <867598193@qq.com>
2018-05-28 20:56:35 +0800

    格式化日期及显示不显示评价

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -5,7 +5,7 @@
         <div class="image-header">
           <img :src="food.image" alt="">
           <div class="back" @click="hide">
-          <i class="icon-arrow_left"></i>
+            <i class="icon-arrow_left"></i>
           </div>
         </div>
         <div class="content">
@@ -45,18 +45,19 @@
           <div class="rating-wrapper">
             <ul v-show="food.ratings && food.ratings.length">
               <!--v-show绑定一个函数-->
-              <li v-show="needShow(rating.rateType,rating.text)" v-for="rating in food.ratings" class="rating-item border-1px">
+              <li v-show="needShow(rating.rateType,rating.text)" v-for="rating in food.ratings"
+                  class="rating-item border-1px">
                 <div class="user">
                   <span class="name">{{rating.username}}</span>
-                  <img  class="avatar" width="12" height="12" :src="rating.avatar" alt="">
+                  <img class="avatar" width="12" height="12" :src="rating.avatar" alt="">
                 </div>
-                <div class="time">{{rating.rateTime}}</div>
+                <div class="time">{{rating.rateTime|formatDate }}</div>
                 <p class="text">
                   <span :class="{'icon-thumb_up':rating.rateType===0,'icon-thumb_down':rating.rateType===1}"></span>{{rating.text}}
                 </p>
               </li>
             </ul>
-            <div class="no-rating" v-show="!food.ratings||!food.ratings.length"></div>
+            <div class="no-rating" v-show="!food.ratings||!food.ratings.length">暂无评价</div>
           </div>
         </div>
       </div>
@@ -67,13 +68,14 @@
 <script>
   import Vue from  'vue'
   import BScroll from 'better-scroll'
+  import {formatDate} from  '../../common/js/date'
   import cartControl from '../cartcontrol/cartcontrol.vue'
   import Split from '../split/split.vue'
   import ratingSelect from '../ratingselect/ratingselect.vue'
 
 
   /*const POSITIVE = 0;
-  const NEGATIVE = 1;*/
+   const NEGATIVE = 1;*/
   const ALL = 2;
   export default{
     props: {
@@ -84,12 +86,12 @@
     data(){
       return {
         showFlag: false,
-        selectType:ALL,
-        onlyContent:true,
-        desc:{
-            all:'全部',
-            positive:'推荐',
-            negative:'吐槽'
+        selectType: ALL,
+        onlyContent: true,
+        desc: {
+          all: '全部',
+          positive: '推荐',
+          negative: '吐槽'
         }
       }
     },
@@ -136,23 +138,31 @@
         });
 
       },
-      needShow(type,text){
-          //如果想只看内容，且没有评价的内容的时候返回false，把没有内容的评论置为false
-          if(this.onlyContent&&!text){
-              return false
-          }
-          if(this.selectType===ALL){
-              return true
-          }else {
-              return type===this.selectType;
-          }
+      needShow(type, text){
+        //如果想只看内容，且没有评价的内容的时候返回false，把没有内容的评论置为false
+        if (this.onlyContent && !text) {
+          return false
+        }
+        if (this.selectType === ALL) {
+          return true
+        } else {
+          return type === this.selectType;
+        }
 
+      },
+
+    },
+    filters: {
+      formatDate(time){
+        let date = new Date(time);
+       // console.log(date);
+        return formatDate(date, 'yyyy-MM-dd hh:mm')
       }
-    } ,
-    components:{
-        cartControl,
-        Split,
-        ratingSelect
+    },
+    components: {
+      cartControl,
+      Split,
+      ratingSelect
     }
   }
   //第59行注释：w3c说给padding值设置为100%的时候，计算的时候是按照这个盒子的宽度（百分百）计算的，所以当我们给padding设置为100%，就相当于宽度的100%，也就是保证上下的padding值和宽度是一样的，所以盒子看起来就是宽高相等的盒子
@@ -169,148 +179,146 @@
     width: 100%
     background: #ffffff
     transform: translate3d(0, 0, 0)
-    &.move-enter-active,&.move-leave-active
-     transition: all 0.2s linear
-    &.move-enter,&.move-leave-active
-     transform: translate3d(100%, 0, 0)
+    &.move-enter-active, &.move-leave-active
+      transition: all 0.2s linear
+    &.move-enter, &.move-leave-active
+      transform: translate3d(100%, 0, 0)
     .food-content
       .image-header
-        position :relative
-        width :100%
-        height :0
-        padding-top :100%
+        position: relative
+        width: 100%
+        height: 0
+        padding-top: 100%
         img
-          position :absolute
-          top:0
-          left :0
-          width :100%
-          height :100%
+          position: absolute
+          top: 0
+          left: 0
+          width: 100%
+          height: 100%
         .back
-          position :absolute
-          top:10px
-          left :0
+          position: absolute
+          top: 10px
+          left: 0
           .icon-arrow_left
-            display :block
-            padding :10px
-            font-size :20px
-            color :#ffffff
-
-
+            display: block
+            padding: 10px
+            font-size: 20px
+            color: #ffffff
 
       .content
-        position :relative
-        padding :18px
+        position: relative
+        padding: 18px
         .title
-          line-height :14px
-          margin-bottom :8px
-          font-size :14px
-          font-weight :700
-          color :rgb(7,17,27)
+          line-height: 14px
+          margin-bottom: 8px
+          font-size: 14px
+          font-weight: 700
+          color: rgb(7, 17, 27)
         .detail
-          margin-bottom :18px
-          line-height :10px
-          font-size :0
-          height:10px
-          .sell-count,.rating
-           font-size :10px
-           color :rgb(147,153,159)
+          margin-bottom: 18px
+          line-height: 10px
+          font-size: 0
+          height: 10px
+          .sell-count, .rating
+            font-size: 10px
+            color: rgb(147, 153, 159)
           .sell-count
-             margin-right :12px
+            margin-right: 12px
         .price
-          font-weight :700
-          line-height :24px
+          font-weight: 700
+          line-height: 24px
         .now
-          margin-right :8px
-          font-size :14px
-          color: rgb(240,20,20)
+          margin-right: 8px
+          font-size: 14px
+          color: rgb(240, 20, 20)
         .old
-          text-decoration :line-through
-          font-size :10px
-          color :rgb(147,153,159)
-
+          text-decoration: line-through
+          font-size: 10px
+          color: rgb(147, 153, 159)
 
         .cartcontrol-wrapper
           position absolute
-          right :12px
-          bottom :12px
+          right: 12px
+          bottom: 12px
         .buy
-          position :absolute
-          right :18px
-          bottom :18px
-          z-index :10px
-          line-height :24px
-          padding :0 12px
-          box-sizing :border-box
-          border-radius :12px
-          color :#fff
-          background :rgb(0,160,220)
-          font-size :10px
+          position: absolute
+          right: 18px
+          bottom: 18px
+          z-index: 10px
+          line-height: 24px
+          padding: 0 12px
+          box-sizing: border-box
+          border-radius: 12px
+          color: #fff
+          background: rgb(0, 160, 220)
+          font-size: 10px
           opacity: 1
           &.fade-enter-active, &.fade-leave-active
-           transition: all 0.2s
+            transition: all 0.2s
           &.fade-enter, &.fade-leave-active
-           opacity: 0
-           z-index: -1
-
+            opacity: 0
+            z-index: -1
 
       .info
-        padding :18px
+        padding: 18px
         .title
-          line-height :14px
-          margin-bottom :16px
-          font-size :14px
-          color :rgb(77,85,93)
+          line-height: 14px
+          margin-bottom: 16px
+          font-size: 14px
+          color: rgb(77, 85, 93)
         .text
-          line-height :24px
-          padding :0 8px
-          font-size :12px
-          color :rgb(77,85,93)
+          line-height: 24px
+          padding: 0 8px
+          font-size: 12px
+          color: rgb(77, 85, 93)
       .rating
-         padding-top :18px
-         .title
-           line-height :14px
-           margin-left :18px
-           font-size :14px
-           color:rgb(7,17,27)
-         .rating-wrapper
-           padding :0 18px
-           .rating-item
-             position :relative
-             padding : 16px 0
-             border-1px(rgba(7,17,27,.1))
-             .user
-               position :absolute
-               right :0
-               top:16px
-               line-height :12px
-               font-size :0
-               .name
-                 display :inline-block
-                 margin-right :6px
-                 vertical-align :top
-                 font-size :10px
-                 color :rgb(147,153,159)
-               .avatar
-                  border-radius :50%
+        padding-top: 18px
+        .title
+          line-height: 14px
+          margin-left: 18px
+          font-size: 14px
+          color: rgb(7, 17, 27)
+        .rating-wrapper
+          padding: 0 18px
+          .rating-item
+            position: relative
+            padding: 16px 0
+            border-1px(rgba(7, 17, 27, .1))
+            .user
+              position: absolute
+              right: 0
+              top: 16px
+              line-height: 12px
+              font-size: 0
+              .name
+                display: inline-block
+                margin-right: 6px
+                vertical-align: top
+                font-size: 10px
+                color: rgb(147, 153, 159)
+              .avatar
+                border-radius: 50%
 
+            .time
+              margin-bottom: 6px
+              line-height: 12px
+              font-size: 10px
+              color: rgb(147, 153, 159)
+            .text
+              line-height: 16px
+              font-size: 12px
+              color: rgb(7, 17, 27)
+              .icon-thumb_up, .icon-thumb_down
+                margin-right: 4px
+                line-height: 16px
+                font-size: 12px
+              .icon-thumb_up
+                color: rgb(0, 160, 220);
+              .icon-thumb_down
+                color: rgb(147, 153, 159)
 
-             .time
-               margin-bottom :6px
-               line-height :12px
-               font-size :10px
-               color :rgb(147,153,159)
-              .text
-                line-height :16px
-                font-size :12px
-                color :rgb(7,17,27)
-                .icon-thumb_up,.icon-thumb_down
-                  margin-right :4px
-                  line-height :16px
-                  font-size :12px
-                .icon-thumb_up
-                  color: rgb(0,160,220);
-                .icon-thumb_down
-                   color :rgb(147,153,159)
-
-
+          .no-rating
+            padding: 16px 0
+            font-size: 12px
+            color: rgb(147, 153, 159)
 </style>
