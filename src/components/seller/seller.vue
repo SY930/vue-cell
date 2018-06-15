c358a8735dc7d464c0c6411018480aed10131b2c
SY930 <867598193@qq.com>
2018-06-15 16:50:32 +0800

    商家信息

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="seller">
+  <div class="seller" ref="seller">
     <div class="seller-content">
       <div class="overview">
         <h1 class="title">{{seller.name}}</h1>
@@ -36,6 +36,19 @@
         <div class="content-wrapper border-1px" >
           <p class="content">{{seller.bulletin}}</p>
         </div>
+        <ul v-if="seller.supports" class="supports">
+          <li class="support-item border-1px" v-for="(item,index) in seller.supports">
+            <span class="icon" :class="classMap[seller.supports[index].type]"></span>
+            <span class="text">{{seller.supports[index].description}}</span>
+          </li>
+        </ul>
+      </div>
+      <Split></Split>
+      <div class="pics">
+        <h1 class="title">商家实景</h1>
+        <ul class="pic-wrapper">
+          <li></li>
+        </ul>
       </div>
     </div>
   </div>
@@ -43,6 +56,7 @@
 <script>
   import Star from '../star/star.vue'
   import Split from '../split/split.vue'
+  import BScroll from  'better-scroll'
 export default{
   props:{
       seller:{
@@ -52,6 +66,29 @@ export default{
   components:{
       Star,
       Split
+  },
+  created(){
+    this.classMap = ['decrease', 'discount', 'guarantee', 'invoice', 'special']
+  },
+  watch:{
+      /*'seller'(){
+          this._initScroll()
+      }*/
+  },
+  mounted(){
+      this.$nextTick(()=>{
+          this._initScroll()
+      })
+  },
+  methods:{
+    _initScroll(){
+        if(!this.scroll){
+          this.scroll = new BScroll(this.$refs.seller, {
+            click: true
+          });
+        }
+
+    }
   }
 }
 </script>
@@ -117,4 +154,36 @@ export default{
      .content-wrapper
         padding 0 12px 10px 12px
         border-1px(rgba(7,17,27,.1))
+        .content
+            line-height :24px
+            font-size :12px
+            color :rgb(240,20,20)
+     .supports
+        .support-item
+          padding 16px 12px
+          border-1px(rgba(7,17,27,.1))
+          font-size 0
+          &:last-child
+            border-none()
+          .icon
+            display :inline-block
+            width :16px
+            height :16px
+            vertical-align :top
+            margin-right :6px
+            background-size :16px 16px
+            background-repeat :no-repeat
+            &.decrease
+              bg-image('./img/decrease_4')
+            &.discount
+              bg-image('./img/discount_4')
+            &.guarantee
+              bg-image('./img/guarantee_4')
+            &.invoice
+              bg-image('./img/invoice_4')
+            &.special
+              bg-image('./img/special_4')
+          .text
+            line-height :16px
+            font-size :12px
 </style>
