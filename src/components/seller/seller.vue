0d65cf32e57de7372362117874ab06e9af9425bf
SY930 <867598193@qq.com>
2018-06-15 15:23:43 +0800

    商家信息

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -1,11 +1,120 @@
 <template>
-  <div>seller</div>
+  <div class="seller">
+    <div class="seller-content">
+      <div class="overview">
+        <h1 class="title">{{seller.name}}</h1>
+        <div class="dec border-1px">
+          <Star :size="36" :score="seller.score"></Star>
+          <span class="text">({{seller.ratingCount}})</span>
+          <span class="text">月售{{seller.sellCount}}单</span>
+        </div>
+        <ul class="remark">
+          <li class="block">
+            <h2>起送价</h2>
+            <div class="content">
+              <span class="stress">{{seller.minPrice}}</span>元
+            </div>
+          </li>
+
+          <li class="block">
+            <h2>商家配送</h2>
+            <div class="content">
+              <span class="stress">{{seller.deliveryPrice}}</span>元
+            </div>
+          </li>
+          <li class="block">
+            <h2>配送时间</h2>
+            <div class="content">
+              <span class="stress">{{seller.deliveryTime}}</span>分钟
+            </div>
+          </li>
+        </ul>
+      </div>
+      <Split></Split>
+      <div class="bulletin">
+        <h1 class="title">公告与活动</h1>
+        <div class="content-wrapper border-1px" >
+          <p class="content">{{seller.bulletin}}</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
+  import Star from '../star/star.vue'
+  import Split from '../split/split.vue'
 export default{
-
+  props:{
+      seller:{
+          type:Object
+      }
+  },
+  components:{
+      Star,
+      Split
+  }
 }
 </script>
 <style type="text/stylus" lang="stylus">
+  @import "../../common/stylus/mixin.styl"
+.seller
+  position: absolute
+  top: 174px
+  bottom: 0
+  left: 0
+  width: 100%
+  overflow: hidden
+  .overview
+    padding :18px
+    .title
+     margin-bottom 8px
+     line-height :14px
+     color :rgb(7,17,27)
+     font-size :14px
+    .dec
+      padding-bottom :18px
+      border-1px(rgba(7,17,27,.1))
+      font-size :0
+      .star
+        display :inline-block
+        margin-right :8px
+        vertical-align: top
+      .text
+        display inline-block
+        margin-right :12px
+        line-height :18px
+        vertical-align: top
+        font-size 10px
+        color :rgb(77,85,93)
 
+    .remark
+      display flex
+      padding-top 18px
+      .block
+        flex 1
+        text-align center
+        border-right 1px solid rgba(7,17,27,.1)
+        &:last-child
+          border-right none
+        h2
+         margin-bottom 4px
+         line-height 14px
+         font-size :10px
+         color rgb(147,153,159)
+        .content
+          line-height 24px
+          font-size 10px
+          color rgb(7,17,27)
+          .stress
+            font-size :24px
+  .bulletin
+     padding 18px 18px 0 18px
+     .title
+       margin-bottom 8px
+       line-height :14px
+       color :rgb(7,17,27)
+       font-size :14px
+     .content-wrapper
+        padding 0 12px 10px 12px
+        border-1px(rgba(7,17,27,.1))
 </style>
